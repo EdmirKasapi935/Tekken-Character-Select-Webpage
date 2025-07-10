82c6ba609224e634a0b52b9cf9b92eab2c053c33
EdmirKasapi935 <kasapiedmir1@gmail.com>
2025-07-10 08:58:45 +0200

    the charcter picture now has animation, adjusted the position of the html elements and also got the character names

--- CharacterSelectProject/index.php
+++ CharacterSelectProject/index.php
@@ -6,55 +6,92 @@ include("inits.php");
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
+
 <body>
-<h1 style="text-align: center;" id="currentCharacterText"> Character </h1>
-
-<img src="" id="currentCharacterPicture" style="display: block; margin-left: auto; margin-right: auto;" >
-
-
-
-<script>
     
-    var characters = <?php echo json_encode($characters) ?>;
-    var current = characters.Kazuya.character;
-    var characterText = document.getElementById("currentCharacterText");
-    var characterImage = document.getElementById("currentCharacterPicture");
-    characterText.innerHTML = current.name;
-    characterImage.src = "CharacterPictures/" + current.name + ".png";
 
-    document.addEventListener('keydown',function(e){
-    switch(e.key){
-        case 'ArrowUp': if(current.up){ current = characters[current.up].character };
-                        characterText.innerHTML = current.name;
-                        characterImage.src = "CharacterPictures/" + current.name + ".png";    
-                        break;
-        case 'ArrowDown': if(current.down){ current = characters[current.down].character };
-                          characterText.innerHTML = current.name;
-                          characterImage.src = "CharacterPictures/" + current.name + ".png";    
-                          break;
-        case 'ArrowLeft': if(current.left){ current = characters[current.left].character };
-                          characterText.innerHTML = current.name;
-                          characterImage.src = "CharacterPictures/" + current.name + ".png";    
-                          break;
-        case 'ArrowRight': if(current.right){ current = characters[current.right].character };
-                           characterText.innerHTML = current.name;
-                           characterImage.src = "CharacterPictures/" + current.name + ".png";    
-                           break;
-    }
-    });
-</script>
+
+    <div style="width: 50%" ;>
+        <img  src="CharacterNames/Name_Alisa.png" style="position: absolute; z-index: 1; margin-top:20%; margin-left:10%" id="currentCharacterName"> 
+        <img src="" id="currentCharacterPicture" style="display: block; margin-top:10%; margin-left: auto; margin-right: auto; position: relative; z-index: -100;" class="">
+    </div>
+    <!--
+offset top:80
+offset left:329
+-->
+
+    <script>
+        var characters = <?php echo json_encode($characters) ?>;
+        var current = characters.Kazuya.character;
+        var characterName = document.getElementById("currentCharacterName");
+        var characterImage = document.getElementById("currentCharacterPicture");
+        characterImage.src = "CharacterPictures/" + current.name + ".png";
+        characterName.src = "CharacterNames/Name_"+current.name+".png";
+        characterImage.classList.add("anim");
+
+        document.addEventListener('keydown', function(e) {
+            switch (e.key) {
+                case 'ArrowUp':
+                    if (current.up) {
+                        current = characters[current.up].character
+                        characterImage.classList.remove("anim");
+                        characterImage.offsetWidth;
+                        characterImage.classList.add("anim");
+                    }
+                    characterName.src = "CharacterNames/Name_"+current.name+".png";
+                    characterImage.src = "CharacterPictures/" + current.name + ".png";
+
+                    break;
+                case 'ArrowDown':
+                    if (current.down) {
+                        current = characters[current.down].character
+                        characterImage.classList.remove("anim");
+                        characterImage.offsetWidth;
+                        characterImage.classList.add("anim");
+                    }
+                    characterName.src = "CharacterNames/Name_"+current.name+".png";
+                    characterImage.src = "CharacterPictures/" + current.name + ".png";
+
+                    break;
+                case 'ArrowLeft':
+                    if (current.left) {
+                        current = characters[current.left].character
+                        characterImage.classList.remove("anim");
+                        characterImage.offsetWidth;
+                        characterImage.classList.add("anim");
+                    }
+                    characterName.src = "CharacterNames/Name_"+current.name+".png";
+                    characterImage.src = "CharacterPictures/" + current.name + ".png";
+
+                    break;
+                case 'ArrowRight':
+                    if (current.right) {
+                        current = characters[current.right].character
+                        characterImage.classList.remove("anim");
+                        characterImage.offsetWidth;
+                        characterImage.classList.add("anim");
+                    }
+                    characterName.src = "CharacterNames/Name_"+current.name+".png";
+                    characterImage.src = "CharacterPictures/" + current.name + ".png";
+
+                    break;
+            }
+        });
+    </script>
 
 </body>
 
 <script>
     if (window.history.replaceState) {
-      window.history.replaceState(null, null, window.location.href);
+        window.history.replaceState(null, null, window.location.href);
     }
 </script>
-</html>
 
+</html>
